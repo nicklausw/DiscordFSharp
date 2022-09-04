83f56b02d991741e9bb471b09109f9c9efc75cc6
nicklausw <n@nicklausw.com>
2022-09-04 14:22:12 -0400

    more consistent code; explain how c# and f# code line up

--- Program.fs
+++ Program.fs
@@ -2,40 +2,39 @@
 open Discord.WebSocket
 open System.Threading.Tasks
 
-let _client = new DiscordSocketClient()
+let client = new DiscordSocketClient()
 
 // run when logged into discord
-let readyTask = task {
+let readyTask() = task {
   printfn "connected to discord"
 }
 
 // run when message received
 let messageReceived(msg: SocketMessage) = task {
   // don't process this bot's messages
-  if msg.Author.Id <> _client.CurrentUser.Id then
+  if msg.Author.Id <> client.CurrentUser.Id then
     printfn $"got a message: {msg.Content}"
     msg.Channel.SendMessageAsync("Got the message").Wait()
 }
 
 // our main async function
-let MainAsync() =
-  async {
-    let token = System.Environment.GetEnvironmentVariable("TEST_BOT")
+let MainAsync() = task {
+  let token = System.Environment.GetEnvironmentVariable("TEST_BOT")
 
-    // think of Wait() as equal to C# await
-    _client.LoginAsync(TokenType.Bot, token).Wait()
-    _client.StartAsync().Wait()
+  // log in
+  client.LoginAsync(TokenType.Bot, token).Wait()
+  client.StartAsync().Wait()
     
-    // add our event functions
-    _client.add_Ready(fun _ -> readyTask)
-    _client.add_MessageReceived(fun msg -> messageReceived(msg))
+  // add our event functions
+  client.add_Ready(fun _ -> readyTask())
+  client.add_MessageReceived(fun msg -> messageReceived(msg))
 
-    // keep running
-    Task.Delay(-1).Wait()
-  }
+  // keep running
+  Task.Delay(-1).Wait()
+}
 
 [<EntryPoint>]
 let main argv =
-  MainAsync() |> Async.RunSynchronously
+  MainAsync().Wait()
   0
 
--- readme.md
+++ readme.md
@@ -1,2 +1,25 @@
 # Discord in F#
 Yep. Uses [Discord.Net.](https://github.com/discord-net/Discord.Net)
+
+## How do F# and C# code line up?
+### Awaiting an async task
+---
+```C#
+// C#
+await client.StartAsync();
+```
+```F#
+// F#
+client.StartAsync().Wait()
+```
+
+### Writing an asynchronous task
+---
+```C#
+// C#
+async Task MessageReceived(SocketMessage msg) { }
+```
+```F#
+// F#
+let MessageReceived(msg: SocketMessage) = task { }
+```
